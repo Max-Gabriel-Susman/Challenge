7f0c86b65f0c5cfe294f7f1141f334392c82d51d
Max_Gabriel_Susman <gabe.susman@gmail.com>
2020-10-19 23:57:48 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+// using System.Collections.Generic;
+
+
+namespace IFit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List<int> cardTypes = new List<int>();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
